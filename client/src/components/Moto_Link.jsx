53117664d4f379e2597add909c840f1cb98b4337
C-NikhilKarthik <nikhilkarthik241103@gmail.com>
2023-09-20 01:51:51 +0530

    integration of chatbot completed

--- client/src/components/Moto_Link.jsx
+++ client/src/components/Moto_Link.jsx
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 
 export default function Moto_Link() {
+  const [videoLink, setVideoLink] = useState(""); // State to store the input value
+  const navigate = useNavigate(); // Initialize the useNavigate hook
+
+  const handleInputChange = (e) => {
+    setVideoLink(e.target.value);
+  };
+
+  const handleButtonClick = () => {
+    // Check if the input value is a valid YouTube link (you can add more validation as needed)
+    if (isValidYouTubeLink(videoLink)) {
+      // Save the input value to localStorage
+      localStorage.setItem("videoLink", videoLink);
+
+      // Redirect to the "/chat" route using the navigate function
+      navigate("/chat");
+    } else {
+      alert("Please enter a valid YouTube link.");
+    }
+  };
+
+  // Function to validate YouTube links
+  const isValidYouTubeLink = (link) => {
+    // You can implement a more robust link validation logic here
+    // For simplicity, this example checks if the link contains "youtube.com"
+    return link.includes("youtube.com");
+  };
+
   return (
     <div className="w-full flex flex-col justify-center items-center md:p-9 mt-9">
       <div className="font-bold flex flex-col items-center gap-2 text-[#223563] mt-6">
@@ -13,10 +41,15 @@ export default function Moto_Link() {
       <div className="mt-10 lg:mt-16 w-full lg:w-[50%] gap-1 flex justify-center">
         <input
           placeholder="Enter the video link"
-          type="link"
+          type="text"
           className="p-2 lg:p-4 border-[2px] md:w-[50%] lg:w-[80%] border-[#3B5594] rounded-lg"
+          value={videoLink}
+          onChange={handleInputChange}
         />
-        <button className="p-2 lg:p-4 bg-[#3B5594] font-bold rounded-lg text-white focus:outline-none text-[10px] lg:text-[18px]">
+        <button
+          onClick={handleButtonClick}
+          className="p-2 lg:p-4 bg-[#3B5594] font-bold rounded-lg text-white focus:outline-none text-[10px] lg:text-[18px]"
+        >
           Get started
         </button>
       </div>
