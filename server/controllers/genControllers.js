1dd1bc4d95448aadca78b36fee8b9a6d98a20b09
Ashish Lal <ashishlal2003@gmail.com>
2023-09-21 01:01:32 +0530

    corrected routes

--- server/controllers/genControllers.js
+++ server/controllers/genControllers.js
@@ -1,31 +1,36 @@
 const { TextServiceClient } = require("@google-ai/generativelanguage").v1beta2;
 
 const { GoogleAuth } = require("google-auth-library");
-const transcript = require("../utility/transcript");
+const { YoutubeTranscript } = require("youtube-transcript");
+
+const { transcript } = require("../utility/transcript");
 
 
 const MODEL_NAME = "models/text-bison-001";
-const API_KEY = "AIzaSyCcrKRaX4Lx5RjcjEaY7IYN1dA7UUTFm1M";
+const API_KEY = "AIzaSyD4kJWgEQXDxOAO7QZLyzyrlKZkwFt2U1E";
 
 const client = new TextServiceClient({
   authClient: new GoogleAuth().fromAPIKey(API_KEY),
 });
 
-const anyPrompt = async(req, res, next) => {
-    try {
-      const { prompt } = req.body;
-      const result = await client.generateText({
-        model: MODEL_NAME,
-        prompt: {
-          text: prompt,
-        },
-      });
-      // const formattedOutput = formattedOutput(result[0].candidates[0].output);
-      res.status(200).json(result[0].candidates[0].output);
-    } catch (error) {
-      res.send(500).json({ message: error.message })
-    }
+const anyPrompt = async (req, res, next) => {
+  try {
+    const { videoURL, prompt } = req.body;
+    const toSummarize = await transcript(videoURL);
+    const toPass = `${prompt}. Answer the question with respect to the following piece of text: ${toSummarize}`;
+    const result = await client.generateText({
+      model: MODEL_NAME,
+      prompt: {
+        text: toPass,
+      },
+    });
+    res.status(200).json(result[0].candidates[0].output);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+  
 };
+
 
 //CORRECT the summarize API by calling the getTranscript function correctly!
 const summarize = async (req, res, next) => {
@@ -50,6 +55,7 @@ const quiz = async(req, res, next) => {
   try{
       const { videoURL } = req.body;
       const toQuiz = await transcript(videoURL);
+      console.log(toQuiz);
       const prompt = `Create a MCQ quiz on the following text: ${toQuiz}`;
       const result = await client.generateText({
         model: MODEL_NAME,
--- server/utility/transcript.js
+++ server/utility/transcript.js
@@ -1,6 +1,7 @@
 const { YoutubeTranscript } = require("youtube-transcript");
 
 const transcript = async (videoURL) => {
+  baahar = videoURL;
   try {
     const result = await YoutubeTranscript.fetchTranscript(videoURL).then((transcript) => {
         const completeText = transcript.map((entry) => entry.text).join(' ');
@@ -13,4 +14,4 @@ const transcript = async (videoURL) => {
   }
 };
 
-module.exports = transcript;
+module.exports = { transcript };
