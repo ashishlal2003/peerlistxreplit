c6c7619273c84dd9410ef816639956ed34f83212
Ashish Lal <ashishlal2003@gmail.com>
2023-09-20 17:06:12 +0530

    changes from lal

--- server/controllers/transcriptControllers.js
+++ server/controllers/transcriptControllers.js
@@ -1,9 +1,8 @@
-const {YouTubeTranscriptApi} = require('youtube_transcript_api');
+const { YoutubeTranscript } = require('youtube-transcript');
 
 const getTranscript = async (req, res) => {
     try {
       const { videoURL } = req.body;
-      console.log(req.body)
       const transcript = await YoutubeTranscript.fetchTranscript(videoURL).then((transcript) => {
         const completeText = transcript.map((entry) => entry.text).join(' ');
         return completeText;
